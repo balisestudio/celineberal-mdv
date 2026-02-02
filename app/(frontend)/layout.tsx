e426921784b3244f2f3dded8823629a07a9d7f49
Yaniv Douieb <yanivdouieb@icloud.com>
2026-02-02 13:44:36 +0100

    feat: implement initial layout and routing structure for frontend and payload admin

---
+++ app/(frontend)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export {RootLayout as default}
---
+++ app/(frontend)/page.tsx
@@ -0,0 +1,5 @@
+const Root = () => {
+  return <></>
+}
+
+export {Root as default}
