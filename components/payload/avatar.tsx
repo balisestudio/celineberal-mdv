87bde820cd29f2e14bf848575af17ca0050d4598
Yaniv Douieb <yanivdouieb@icloud.com>
2026-02-15 15:13:17 +0100

    fix: reduce Avatar component dimensions for improved layout

--- components/payload/avatar.tsx
+++ components/payload/avatar.tsx
@@ -7,8 +7,8 @@ export const Avatar = () => {
 		// biome-ignore lint/a11y/useAltText: unnecessary
 		<img
 			src={`https://api.dicebear.com/9.x/glass/svg?seed=${user?.id}`}
-			width={32}
-			height={32}
+			width={25}
+			height={25}
 			style={{ borderRadius: "50%" }}
 		/>
 	);
