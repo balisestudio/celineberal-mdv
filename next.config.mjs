6f581ee7c630281bf4e9b0e672ef36979f9604b6
Yaniv Douieb <yanivdouieb@icloud.com>
2026-02-27 21:42:21 +0100

    chore: update dependencies and config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+		serverActions: {
+			bodySizeLimit: "4mb",
+		},
+		globalNotFound: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+};
 
 export default withPayload(nextConfig, { devBundleServerPackages: false });
