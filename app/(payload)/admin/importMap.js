ba19c1f6bf074c93fc1915ecf3d732f255559595
Yaniv Douieb <yanivdouieb@icloud.com>
2026-03-03 01:27:48 +0100

    build: update import map and custom styles

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -4,8 +4,11 @@ import {
 	Icon as Icon_33afbe71c4366a5ad65aaf4d6b62ddb4,
 	Logo as Logo_33afbe71c4366a5ad65aaf4d6b62ddb4,
 } from "../../../components/payload/graphics";
+import { ImportsLots as ImportsLots_148b40f15d4b80691e3177847c4a7ed0 } from "../../../components/payload/imports-lots";
 
 export const importMap = {
+	"/components/payload/imports-lots#ImportsLots":
+		ImportsLots_148b40f15d4b80691e3177847c4a7ed0,
 	"/components/payload/avatar#Avatar": Avatar_8501e0134e041fe529df17c38dc04676,
 	"/components/payload/graphics#Icon": Icon_33afbe71c4366a5ad65aaf4d6b62ddb4,
 	"/components/payload/graphics#Logo": Logo_33afbe71c4366a5ad65aaf4d6b62ddb4,
