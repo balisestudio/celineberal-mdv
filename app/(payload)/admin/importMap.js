07adf65bda68302571150fc4b5d0aa5d7d607dc6
Yaniv Douieb <yanivdouieb@icloud.com>
2026-03-03 18:41:21 +0100

    chore: update admin import map formatting

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -1,17 +1,13 @@
-import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from "@payloadcms/next/rsc";
-import { Avatar as Avatar_8501e0134e041fe529df17c38dc04676 } from "../../../components/payload/avatar";
-import {
-	Icon as Icon_33afbe71c4366a5ad65aaf4d6b62ddb4,
-	Logo as Logo_33afbe71c4366a5ad65aaf4d6b62ddb4,
-} from "../../../components/payload/graphics";
-import { ImportsLots as ImportsLots_148b40f15d4b80691e3177847c4a7ed0 } from "../../../components/payload/imports-lots";
+import { ImportsLots as ImportsLots_148b40f15d4b80691e3177847c4a7ed0 } from '../../../components/payload/imports-lots'
+import { Avatar as Avatar_8501e0134e041fe529df17c38dc04676 } from '../../../components/payload/avatar'
+import { Icon as Icon_33afbe71c4366a5ad65aaf4d6b62ddb4 } from '../../../components/payload/graphics'
+import { Logo as Logo_33afbe71c4366a5ad65aaf4d6b62ddb4 } from '../../../components/payload/graphics'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
-	"/components/payload/imports-lots#ImportsLots":
-		ImportsLots_148b40f15d4b80691e3177847c4a7ed0,
-	"/components/payload/avatar#Avatar": Avatar_8501e0134e041fe529df17c38dc04676,
-	"/components/payload/graphics#Icon": Icon_33afbe71c4366a5ad65aaf4d6b62ddb4,
-	"/components/payload/graphics#Logo": Logo_33afbe71c4366a5ad65aaf4d6b62ddb4,
-	"@payloadcms/next/rsc#CollectionCards":
-		CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
-};
+  "/components/payload/imports-lots#ImportsLots": ImportsLots_148b40f15d4b80691e3177847c4a7ed0,
+  "/components/payload/avatar#Avatar": Avatar_8501e0134e041fe529df17c38dc04676,
+  "/components/payload/graphics#Icon": Icon_33afbe71c4366a5ad65aaf4d6b62ddb4,
+  "/components/payload/graphics#Logo": Logo_33afbe71c4366a5ad65aaf4d6b62ddb4,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+}
